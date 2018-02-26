49969f2db42e5a1877c27c5bac5492c388ec6540
sanja081107 <sanja081107@gmail.com>
2018-02-26 15:58:07 +0300

    implement-task-4

---
+++ homeworks/AlexandrMisyuta/4/script.js
@@ -0,0 +1,7 @@
+var a = Number(prompt("Введите 1ое число"));
+a;
+var b = Number(prompt("Введите 2ое число"));
+b;
+var c = Number(prompt("Введите 3ее число"));
+c;
+alert ('a+b-c = ' +(a + b - c));
