aa95de7d22ba377224b0b2193341216bb0b54817
vikalepna <viki@tut.by>
2018-03-06 23:27:04 +0300

    task 6. full.

--- homeworks/ViktoriaGrishanovich/6/js/my.js
+++ homeworks/ViktoriaGrishanovich/6/js/my.js
@@ -4,6 +4,7 @@ alert(filter(arr, function(a) {
   return a % 2 == 0
 })); // 2,4,6
 
+
 alert( filter(arr, inBetween(3, 6)) ); // 3,4,5,6
 
 alert( filter(arr, inArray([1, 2, 10])) ); // 1,2
@@ -20,13 +21,28 @@ function filter(inData, func){
     return outData;
 }
 
+
 function inBetween(firstNumber, lastNumber){
     
-    // ???
+    return function (a) {
+        if(a>=firstNumber && a<=lastNumber)
+            return true;
+        else
+            return false;
+    }; 
 }
 
-
-
+function inArray (arr) {
+    
+    return function(a) {
+        for(var i=0; i<arr.length; i++)
+        {
+            if(a===arr[i]) return true;
+        }
+        return false;
+    }
+    
+}
 
 function sum(arr) {
   return arr.reduce(function(a, b) {
